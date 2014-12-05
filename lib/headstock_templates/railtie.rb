8a782fc81199b7bd7b5b7bf5b740cb7bc3c7924b
George Millo <georgejulianmillo@gmail.com>
2014-12-05 12:33:37 +0000

    removing puts statement

--- lib/headstock_templates/railtie.rb
+++ lib/headstock_templates/railtie.rb
@@ -1,7 +1,6 @@
 module HeadstockTemplates
   class Railtie < Rails::Railtie
     config.app_generators do |g|
-      puts File::expand_path('../templates', __FILE__)
       g.templates.unshift File::expand_path('../../templates', __FILE__)
     end
   end
